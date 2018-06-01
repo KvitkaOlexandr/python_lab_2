162df5857f93ac1b77b792417e0792bdb819b146
Kvitka_Olexandr <sanya-kvitka@ukr.net>
2018-06-01 17:55:51 +0300

    half-working

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from db import Mongo_URL
+import json
+import subprocess
+
+app = Flask(__name__)
+client = MongoClient(Mongo_URL)
+lim = 30
+
+
+@app.route('/')
+def index():
+    page = request.args.get('page')
+    if page is None:
+        page = 0
+    data = client.lab_2.data.find().skip(int(page) * lim).limit(lim)
+    pages = client.lab_2.data.count() // lim
+    return render_template('index.html', data=[d for d in data], page=int(page), pages=pages)
+
+
+@app.route('/<topic>/<author>')
+def topic_author(topic, author):
+    data = client.lab_2.data
+    rec = data.find({'topic': topic, 'author': author})
+    author_messages_count = data.count({'topic': topic, 'author': author})
+    topic_messages_count = data.count({'topic': topic})
+    return render_template('topic_author.html', data=[d for d in rec], author_messages_count=author_messages_count,
+                           topic_messages_count=topic_messages_count)
+
+
+@app.route('/add')
+def add():
+    #spider_name = "coin_spider.py"
+    #subprocess.check_output(['scrapy', 'runspider', spider_name, "-o", "out.json"])
+    with open("out.json") as items_file:
+        result = json.loads(items_file.read())
+        print(result)
+        for r in result:
+            client.cselab.data.insert(r)
+    client.close()
+    return "helloworld"
+
+
+if __name__ == '__main__':
+    app.run()
